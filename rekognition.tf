649d1f415c9115b64d9f06ae1877efd026818544
Maximiliano Aguirre <maximiliano.aguirre@outlook.es>
2022-11-16 11:43:01 -0300

    added rekognition collection (using cloudformation stack)
    Signed-off-by: Maximiliano Aguirre <maximiliano.aguirre@outlook.es>

---
+++ rekognition.tf
@@ -0,0 +1,10 @@
+# Workaround to create Rekognition resources
+# https://github.com/hashicorp/terraform-provider-aws/pull/26053
+resource "aws_cloudformation_stack" "rekognition" {
+  name = "${local.dash_prefix}photo-album-rekognition"
+  tags = var.tags
+
+  template_body = templatefile("${path.module}/cloudformation/rekognition.yaml", {
+    collection_name = "${local.dash_prefix}photo-album"
+  })
+}
